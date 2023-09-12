8753fec833e712c345ef77e91bf747b51359041c
ANKIT KUMAR JHA <129272451+ankit763170@users.noreply.github.com>
2023-09-13 00:36:19 +0530

    Add files via upload

---
+++ controllers/auth.js
@@ -0,0 +1,167 @@
+const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
+const crypto = require('crypto')
+const User = require('../models/user');
+const user = require('../models/user');
+exports.postReset = (req,res,next) =>{
+crypto.randomBytes(32,(err,buffer)=>{
+if(err){
+  console.log(err)
+  return res.redirect('/reset')
+}
+const token = buffer.toString('hex')
+user.findOne({
+  email : req.body.email
+}).then(user=>{
+  if(!user){
+    req.flash('error ','no user find for this user using this email....!')
+    res.redirect('/reset')
+  }
+  user.resetToken = token;
+  user.resetTokenExpiration = Date.now()+3600000;
+  return user.save();
+}).then(result=>{
+res.redirect('/')
+transporter.sendMail({
+  to : req.body.email,
+  from : 'ankitkjha8078@gmail.com',
+  subject: "Password reset",  // Subject line
+  html : `
+<p>you requested to password reset</p>
+<p>click this link <a href="http://localhost/reset/${token}">link</a> to reset password </p>
+`
+})
+}).catch(err=>{
+ console.log(err)
+})
+})
+};
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: '****************************',
+    pass: '***************'
+  }
+});
+
+
+exports.getLogin = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    errorMessage: message
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  let message = req.flash('error');  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    errorMessage: message
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
+    .then(user => {
+      if (!user) {
+        req.flash('error', 'Invalid email or password.');
+        return res.redirect('/login');
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then(doMatch => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save(err => {
+              console.log(err);
+              res.redirect('/');
+            });
+          }
+          req.flash('error', 'Invalid email or password.');
+          res.redirect('/login');
+        })
+        .catch(err => {
+          console.log(err);
+          res.redirect('/login');
+        });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({ email: email })
+    .then(userDoc => {
+      if (userDoc) {
+        req.flash(
+          'error',
+          'E-Mail exists already, please pick a different one.'
+        );
+        return res.redirect('/signup');
+      }
+      return bcrypt
+        .hash(password, 12)
+        .then(hashedPassword => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] }
+          });
+          return user.save();
+        })
+        .then(result => {
+          res.redirect('/login');
+          return transporter.sendMail({
+            to: email,
+            from: 'ankitkjha8078@gmail.com',
+            subject: 'Signup succeeded!',
+            html: '<h1>You successfully signed up!</h1>'
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
+};
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'Reset Password',
+    errorMessage: message
+  });
+};
